615f8746344a7989b52a2bcf47e5b511d67d8960
Thomas Basche <t0m8a5ch3@gmail.com>
2018-07-05 23:57:25 +1000

    dang ol whitespace

--- reposit/auth/__init__.py
+++ reposit/auth/__init__.py
@@ -8,12 +8,11 @@ from reposit.auth.connect import RPConnection
 
 logger = logging.getLogger(__name__)
 
-
 """
 Do some basic checks on the python version. We don't wan't anyone using
 a deprecated version of Python, (older than 2.7). We also log a warning to
 tell a consumer to use Python 3 as it would be good to stop supporting 2
-completely. :) 
+completely. :)
 """
 if sys.version_info < (2, 7):
     raise Exception('Versions of Python older than 2.7 are not supported.')
