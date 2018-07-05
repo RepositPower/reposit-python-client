3e3860749950b0ca6f656a3dc6ddec3d07542730
Thomas Basche <t0m8a5ch3@gmail.com>
2018-07-05 23:49:30 +1000

    Log if using python 2

--- reposit/auth/__init__.py
+++ reposit/auth/__init__.py
@@ -3,6 +3,9 @@ Shortcut imports + checks for python versions.
 """
 from __future__ import absolute_import
 import sys
+import logging
+
+logger = logging.getLogger(__name__)
 
 from reposit.auth.connect import RPConnection
 
@@ -10,6 +13,6 @@ if sys.version_info < (2, 5):
     raise Exception('Versions of Python older than 2.5 are not supported.')
 
 if sys.version_info[0] != 3:
-    raise DeprecationWarning(
+    logger.warning(
         'Python 2 is still supported, but it is recommended you adopt Python 3.'
     )
