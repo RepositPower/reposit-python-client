83d0671d763942a2688c03db064108ac53bac4a9
Tom Basche <tcbasche@gmail.com>
2018-08-21 08:23:09 +1000

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="reposit",
-    version="0.2b1",
+    version="0.3",
     author="Thomas Basche",
     author_email="thomas.basche@repositpower.com",
     description="A library to communicate with a Reposit Controller",
